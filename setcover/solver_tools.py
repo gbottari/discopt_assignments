79f0fcbb5f796b7ff4ac0303b52fd597b0fda47f
Giulio Bottari <giulio.bottari@tvglobo.com.br>
2017-09-27 19:20:30 -0300

    Code cleanup.

--- setcover/solver_tools.py
+++ setcover/solver_tools.py
@@ -3,7 +3,6 @@ sys.path.append('..')
 
 import logging
 import pymzn
-#from ortools.constraint_solver import pywrapcp  # todo: remove this
 from collections import namedtuple
 from tools.solver_tools import Solution, Solver
 from typing import List
@@ -64,52 +63,6 @@ class SCSolver(Solver):
 
     def _solve(self, input_data):
         raise NotImplementedError()
-
-
-# class OrToolsCPSCSolver(SCSolver):
-#     def _solve(self, problem: SCProblem):
-#         logger = logging.getLogger('solver')
-#
-#         solver: pywrapcp.Solver = pywrapcp.Solver("set_cover")
-#         set_count = len(problem.sets)
-#         item_count = len(problem.items)
-#
-#         # Decision vars for selecting the set i
-#         x = [solver.IntVar(0, 1, "x_{:02d}".format(i)) for i in range(set_count)]
-#         c = [solver.IntConst(problem.sets[i].cost, "c_{:02d}".format(i)) for i in range(set_count)]
-#
-#         # Minimizes the cost of the sum of the selected sets.
-#         solver.Minimize(solver.Sum([x[i] * c[i] for i in range(set_count)]), step=1)
-#
-#         # Each item must be covered by at least one of the sets specified
-#         covered_by = [[] for _ in range(item_count)]
-#         for i in range(set_count):
-#             for item in problem.sets[i].items:
-#                 covered_by[item].append(x[i])
-#
-#         for i in range(item_count):
-#             solver.Add(solver.Max(covered_by[i]) == 1)
-#
-#         # Dumb search:
-#         db = solver.Phase(x, solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
-#
-#         solution = SCSolution(problem=problem)
-#         solution.optimal = False
-#         # best_solution: SCSolution = None
-#
-#         # solver.NewSearch(db)
-#         # while solver.NextSolution():
-#         #     solution.selections = [x[i].Value() for i in range(set_count)]
-#         #     if not best_solution or best_solution.get_value() > solution.get_value():
-#         #         best_solution = solution.copy()
-#         # solver.EndSearch()
-#
-#         #assignment = solver.Assignment()
-#         collector: pywrapcp.SolutionCollector = solver.BestValueSolutionCollector(False)
-#         solver.Solve(db, [collector])
-#         print(collector.Solution(0))
-#
-#         return solution
 
 
 class CPSCSolver(SCSolver):
